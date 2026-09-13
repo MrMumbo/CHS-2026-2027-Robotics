1ba8bd11f8b35890833b51c4405fc561cc4cbb25
MrMumbo <danielemerson321@gmail.com>
2026-09-12 20:07:01 -0700

    Initial import: FTC SDK, RoadRunner & tools
    Add complete project skeleton and libraries for the FTC Robot Controller app. Includes: top-level build & gradle wrapper, .gitignore, .gitattributes, LICENSE, README, CONTRIBUTING and PR template. Adds FtcRobotController and TeamCode modules, RoadRunner integration (drive, trajectorysequence, utilities, tuners, localizer), MeepMeepTesting module, logging/dashboard helpers, and supporting resources (manifests, assets, libs, keystore). Sets up TeamCode build scripts and dependencies for RoadRunner, Jackson, Commons Math, and FTC libs. This commit seeds the repo with sample opmodes, tools, and build configuration for development.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Classes/hw.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Classes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class hw extends OpMode {
+    public DcMotor frontRight;
+    public DcMotor frontLeft;
+    public DcMotor backRight;
+    public DcMotor backLeft;
+
+    public void init() {
+        hardwareMap = new HardwareMap(DcMotor, "FrontRight");
+    }
+
+    public void loop() {
+
+    }
+}
